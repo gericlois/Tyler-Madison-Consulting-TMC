267379fd428b56d30da66eecc1e8cf14ba669687
gericlois <gericlois.casugay@lorma.edu>
2025-04-08 23:02:09 +0800

    Added Features:Filled Option to Job

--- domain/pages/scripts/job-fill.php
+++ domain/pages/scripts/job-fill.php
@@ -15,8 +15,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $updateApplicationSql = "UPDATE jobapplications SET progress = 6, status = 2 WHERE job_id = ? AND employee_id = ?";
     $stmt2 = $conn->prepare($updateApplicationSql);
     $stmt2->bind_param("ii", $job_id, $employee_id);
-
+    
+    
     if ($stmt1->execute() && $stmt2->execute()) {
+        $admin_id = $_SESSION['admin_id']; // Assuming admin ID is stored in session
+        $action = "Job Posting Filled";
+        $activity_description = "Filled job posting ID {$job_id}, employee_id '{$employee_id}'.";
+
+        $activity_stmt = $conn->prepare("INSERT INTO activity (user_id, action, description) VALUES (?, ?, ?)");
+        $activity_stmt->bind_param("iss", $admin_id, $action, $activity_description);
+        $activity_stmt->execute();
+        $activity_stmt->close();
+        
         header("Location: ../jobs.php?success=JobFilled");
         exit();
     } else {
